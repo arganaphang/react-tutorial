94f6fb6919dcc6165756379593c7c77ff6b90c37
Argana Phangquestian <arganaphangquestian@gmail.com>
2024-03-23 17:56:18 +0700

    add: state management

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,17 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Button from "./components/button";
+import { useCountStore } from "./stores/count";
 
-function App() {
-  const [count, setCount] = useState(0)
+export default function App() {
+  const { count, increment, decrement } = useCountStore((state) => ({
+    ...state,
+  }));
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Button onClick={() => decrement(2)}>-</Button>
+      <h1>Count {count}</h1>
+      <Button onClick={() => increment(2)}>+</Button>
+      <h1>Power {count * count}</h1>
     </>
-  )
+  );
 }
-
-export default App
---
+++ src/components/button.tsx
@@ -0,0 +1,5 @@
+interface Props extends React.ButtonHTMLAttributes<HTMLButtonElement> {}
+
+export default function Button({ children, ...props }: Props) {
+  return <button {...props}>{children}</button>;
+}
